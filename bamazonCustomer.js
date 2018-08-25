2bae0cbddf2840795edc3744864e26da5c3e22be
Bryan C <bryan.carvalho.design@gmail.com>
2018-08-25 13:25:45 -0400

    Add Manager

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,21 +11,30 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    catalog();
+    productList();
 });
 
-function catalog() {
+function productList() {
     let query = "SELECT * FROM products";
 
     connection.query(query, function (err, res) {
         if (err) throw err;
         for (let i = 0; i < res.length; i++) {
-            console.log(`\nID: ${res[i].item_id}\n`, `Name: ${res[i].product_name}\n`, `Department: ${res[i].department_name}\n`, `Price: $${res[i].price}`);
+            console.log(`\nID: ${res[i].item_id}\n`, `Product Name: ${res[i].product_name}\n`, `Department: ${res[i].department_name}\n`, `Price: $${res[i].price}`);
         }
         console.log(`\n-------------------`);
+
         customerOrder();
     })
 
+}
+
+function validation(value) {
+    if (isNaN(value) === false) {
+        return true;
+    } else {
+        return 'Enter a number';
+    }
 }
 
 function customerOrder() {
@@ -34,45 +43,34 @@ function customerOrder() {
             name: "purchase",
             type: "input",
             message: "Welcome to Bamazon! What is the the ID of the product they would like to buy?",
-            validate: function (value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                return false;
-            }
+            validate: validation
         },
         {
             name: "amount",
             type: "input",
             message: "How many would you like to buy?",
-            validate: function (value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                return false;
-            }
+            validate: validation
         }
     ])
         .then(function (answers) {
-            console.log(`\n-------------------`);
-            let query = `SELECT price, stock_quantity, product_sales FROM products WHERE item_id = ${answers.purchase}`;
-
-            connection.query(query, function (err, res) {
+            console.log('____________________________________________________')
+            let queryOne = "SELECT * FROM products WHERE ? ";
+            connection.query(queryOne, { item_id: answers.purchase }, function (err, res) {
                 if (err) throw err;
-
                 if (answers.amount > res[0].stock_quantity) {
-                    console.log('Insufficient quantity!');
+                    console.log('Sorry! We do not have enough stock to fulfill your request.');
+                    console.log('____________________________________________________')
                 } else {
                     let stock = res[0].stock_quantity - answers.amount;
                     let totalCost = res[0].price * answers.amount;
-                    let revenue = res[0].product_sales + totalCost;
-                    let query = `UPDATE products SET stock_quantity = ${stock}, product_sales = ${revenue} WHERE item_id = ${answers.purchase}`;
+                    let query = "UPDATE products SET stock_quantity = " + stock + " WHERE item_id = " + answers.purchase;
 
                     connection.query(query, function (err, res) {
                         if (err) throw err;
                     })
-
-                    console.log(`You have successfully made your purchase! The total cost is $${parseFloat(totalCost).toFixed(2)}.`);
+                    console.log("Your order: " + res[0].product_name + " | $" + res[0].price);
+                    console.log("Your Order has been fulfilled! The total cost is $" + parseFloat(totalCost).toFixed(2));
+                    console.log('____________________________________________________')
                 }
                 connection.end();
             })
